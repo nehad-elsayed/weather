798b89bc672b7fc856807f3d025dc6d0c3854e5e
nehad-elsayed <nehad.boody74@gmail.com>
2024-12-10 03:10:30 +0200

    final

--- js/index.js
+++ js/index.js
@@ -45,11 +45,10 @@ async function getCountry(country) {
   try {
     let response = await fetch(
       `https://api.weatherapi.com/v1/forecast.json?key=beaa6830775a4e99910131438220406&q=${country}07112&days=3`
-    //   http://api.weatherapi.com/v1/forecast.json?key=19ee02755d0f4ab7ae4231651240412&q=${country}&days=3
+      //   http://api.weatherapi.com/v1/forecast.json?key=19ee02755d0f4ab7ae4231651240412&q=${country}&days=3
     );
 
     finalResponse = await response.json();
-    console.log(finalResponse);
     displayData();
   } catch (error) {
     console.log(error);
